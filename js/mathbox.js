a60e37c03a19dd81d8e2aa19e8d8f1d24330c908
vagrant <vagrant@localhost.localdomain>
2020-12-01 18:34:58 -0600

    update

--- js/mathbox.js
+++ js/mathbox.js
@@ -1,4 +1,9 @@
-function randomNumberLessThan(n) {
-    var x = Math.floor(Math.random() * n);
+function randomNumberLessThan(n, mode) {
+    var x = 0;
+    if (mode === "linear") {
+        x = Math.floor(Math.random() * n);
+    } else {
+        x = Math.floor(Math.sqrt(Math.random()) * n);
+    }
     return x;
 }
