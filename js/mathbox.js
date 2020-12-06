6cb4541ef3fafa2ab156b87f30a3b07add575a5d
vagrant <vagrant@localhost.localdomain>
2020-12-06 12:06:32 -0600

    update

--- js/mathbox.js
+++ js/mathbox.js
@@ -16,3 +16,14 @@ function randomNumberBetween(start, end, mode) {
     var x = Math.floor(rn * (end - start + 1)) + start;
     return x;
 }
+
+function randomSwitch() {
+    var rn = Math.floor(Math.random() * 2);
+    return (rn === 1);
+}
+
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+           !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
